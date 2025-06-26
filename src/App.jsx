f6f5fea73f363f7333bac8dd9331cb21070acdfd
Asrar Othman - AO <asrarothman@gmail.com>
2025-06-27 02:49:54 +0800

    update0.6

--- src/App.jsx
+++ src/App.jsx
@@ -14,43 +14,42 @@ function App() {
         {/* <Login/> */}
         <div className="mother1x">
           <div className="main1x">
-            <div>
-              <div className="header">
-                <div className="header1">
-                  <img
-                    src="/logo-pertanian.png" // atau hanya "logo.jpeg" pun boleh
-                    height={30}
-                    alt="logo"
-                  />
+            <div className="header">
+              <div className="header1">
+                <img
+                  src="/logo-pertanian.png" // atau hanya "logo.jpeg" pun boleh
+                  height={30}
+                  alt="logo"
+                />
+              </div>
+              <div>
+                <div style={{ fontSize: "20px" }} className="tajuk">
+                  <a>Sensori 2025</a>
                 </div>
-                <div>
-                  <div style={{ fontSize: "20px" }} className="tajuk">
-                    <a>Sensori 2025</a>
-                  </div>
-                </div>
+              </div>
 
-                <div className="header2">
-                  <img
-                    src="/menu.png" // atau hanya "logo.jpeg" pun boleh
-                    height={25}
-                    alt="logo"
-                  />
-                </div>
-              </div>
-              <div className="sample1">
-                <a style={{ fontSize: "16px" }}>Sample 1234 - karipap</a>
-              </div>
-              <div className="img">
-                <div className="img1">
-                  <img
-                    src="/img1.jpeg" // atau hanya "logo.jpeg" pun boleh
-                    height={"100%"}
-                    alt="logo"
-                    style={{ borderRadius: "10px" }}
-                  />
-                </div>
+              <div className="header2">
+                <img
+                  src="/menu.png" // atau hanya "logo.jpeg" pun boleh
+                  height={25}
+                  alt="logo"
+                />
               </div>
             </div>
+            <div className="sample1">
+              <a style={{ fontSize: "16px" }}>Sample 1234 - karipap</a>
+            </div>
+            <div className="img">
+              <div className="img1">
+                <img
+                  src="/img1.jpeg" // atau hanya "logo.jpeg" pun boleh
+                  height={"100%"}
+                  alt="logo"
+                  style={{ borderRadius: "10px" }}
+                />
+              </div>
+            </div>
+            <div></div>
           </div>
 
           <div className="main2x">
